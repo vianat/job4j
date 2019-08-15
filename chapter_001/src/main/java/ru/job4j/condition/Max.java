1b9c74abc66ea19567f6a6a2a9a37bb50bc46d1c
vianat <nik.sergey87@mail.ru>
2019-08-15 12:09:31 -0700

    Max of 2

---
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,8 @@
+package ru.job4j.condition;
+
+public class Max {
+    public int max(int left,int right){
+        int result = left == right ? right : (left > right ? left : right);
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+    @Test
+    public void whenLeftLessThenRight() {
+        Max max = new Max();
+        int result = max.max(0, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenLeftMoreThenRight() {
+        Max max = new Max();
+        int result = max.max(2, 0);
+        assertThat(result, is(2));
+    }
+
+    public void whenLeftEqualRight() {
+        Max max = new Max();
+        int result = max.max(2, 2);
+        assertThat(result, is(2));
+    }
+
+}
