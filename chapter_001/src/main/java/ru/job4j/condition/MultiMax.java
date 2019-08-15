0f77f87ddc57eeebd4d8f89803433999a344542c
vianat <nik.sergey87@mail.ru>
2019-08-15 16:49:12 -0700

    Max of 3

---
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+public class MultiMax {
+    public int max(int first, int second, int third) {
+        int result;
+        result =  first > second & first > third ? first : second > first & second > third ? second : third;
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MultiMaxTest {
+    @Test
+    public void whenSecondMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(1, 4, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenFirstMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(11, 4, 2);
+        assertThat(result, is(11));
+    }
+
+    @Test
+    public void whenThirdtMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(11, 4, 22);
+        assertThat(result, is(22));
+    }
+
+    @Test
+    public void whenAllEuals() {
+        MultiMax check = new MultiMax();
+        int result = check.max(33, 33, 33);
+        assertThat(result, is(33));
+    }
+}
