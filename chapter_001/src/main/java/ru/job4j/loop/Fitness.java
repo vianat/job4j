21f371f21fc46eb3eb1ec555470a14efa3af5eb5
vianat <nik.sergey87@mail.ru>
2019-08-16 16:32:40 -0700

    Протеиновая диета.[#169118]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public int calc(int ivan, int nik) {
+        int month = 0;
+        while (true){
+            if(ivan > nik){
+                break;
+            }
+            ivan *= 3;
+            nik *= 2;
+            month++;
+        }
+        return month;
+    }
+}
