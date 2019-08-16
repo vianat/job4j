6deaf5de81e97954ee2f730c0daf146bc009e72c
vianat <nik.sergey87@mail.ru>
2019-08-15 22:53:27 -0700

    fix ma  ma

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -3,7 +3,9 @@ package ru.job4j.condition;
 public class MultiMax {
     public int max(int first, int second, int third) {
         int result;
-        result =  first > second & first > third ? first : second > first & second > third ? second : third;
+        if (first > second & first > third) result = first;
+        else if (second > first & second > third) result = second;
+        else result = third;
         return result;
     }
 }
