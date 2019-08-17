bf89b9a1f6190369a6a0f4316c9c0faa145a4226
vianat <nik.sergey87@mail.ru>
2019-08-17 16:59:37 -0700

    JavaDoc. Code convention[#169116]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,21 +1,53 @@
 package ru.job4j.calculator;
 
+/*
+* Class Calculator производит вычисления над числами
+* @author Sergei Nikiforov
+* @since 13.08.2019
+* @version 1
+* */
+
+
 public class Calculator {
 
+    /*
+    * Сложение 2х чисел
+    * @param first - первый аргумент
+    * @param second - второй аргумент
+    * @return результат
+    * */
     public static void add(double first, double second) {
         double result =  first + second;
         System.out.println(first + " + " + second + " = " + result);
     }
 
+    /*
+     * Деление 2х чисел
+     * @param first - первый аргумент
+     * @param second - второй аргумент
+     * @return результат
+     * */
     public static void div(double first, double second) {
         double result =  first / second;
         System.out.println(first + " / " + second + " = " + result);
     }
 
+    /*
+     * Умножение 2х чисел
+     * @param first - первый аргумент
+     * @param second - второй аргумент
+     * @return результат
+     * */
     public static void multiply(double first, double second) {
         double result =  first * second;
         System.out.println(first + " * " + second + " = " + result);
     }
+    /*
+     * Вычитание 2х чисел
+     * @param first - первый аргумент
+     * @param second - второй аргумент
+     * @return результат
+     * */
     public static void subtrack(double first, double second) {
         double result =  first - second;
         System.out.println(first + " - " + second + " = " + result);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Calculator - производит вычесления
+ * Fit - вычесляет идеальный вес на основе роста и пола
+ * @author Sergei Nikiforov (vianatdev@gmail.com)
+ * @since 13.08.2019
+ * @version 1
+ * */
+package ru.job4j.calculator;
