9ad16bf607155dd380820e14d346b2e8458f6ffb
vianat <nik.sergey87@mail.ru>
2019-08-18 13:22:33 -0700

    Массив заполнен true или false[#169126]

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+import java.util.Arrays;
+/*
+ * Class Check проверяет различие элементов в массиве
+ * @author Sergei Nikiforov
+ * @since 18.08.2019
+ * @version 1
+ * */
+public class Check {
+    /*
+     * проверяет что в массиве все элементы равны
+     * @param data - принимаемый массив
+     * @return true - если все эелменты равны, иначе false
+     * */
+    public boolean mono(boolean[] data) {
+        System.out.println(Arrays.toString(data));
+        boolean result = false;
+        int trueCount = 0, falseCount = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == true) trueCount++;
+            else falseCount++;
+        }
+        if (falseCount == 0) result = true;
+        return result;
+    }
+}
