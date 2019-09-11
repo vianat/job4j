22256d1a1699c08decd56b9ce0d94aa7410651b8
vianat <nik.sergey87@mail.ru>
2019-09-11 00:45:12 -0700

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,5 +1,6 @@
 package ru.job4j.tracker;
 
+import org.hamcrest.core.Is;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,11 +16,13 @@ public class StartUITest {
     private final PrintStream stdout = System.out;
     // буфер для результата.
     private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private StartUITest tracker;
 
     @Before
     public void beforeTests() {
         System.out.println("@before method");
         System.setOut(new PrintStream(this.out));
+
     }
 
     @After
@@ -38,8 +41,48 @@ public class StartUITest {
         // создаём StartUI и вызываем метод init()
         new StartUI(input).init();
         // проверяем, что введённое при эмуляции.
-        System.out.println(new String(out.toByteArray()));
-        assertThat(tracker.findAll(), is(tracker.findByName(item.getName())));
+        // System.out.println(new String(out.toByteArray()));
+        assertThat(
+                new String(out.toByteArray()),
+                Is.is(
+                        new StringBuilder()
+                                .append("Меню :")
+                                .append(System.lineSeparator())
+                                .append("0 - Добавить заявку")
+                                .append(System.lineSeparator())
+                                .append("1 - Показать все заявки")
+                                .append(System.lineSeparator())
+                                .append("2 - Изменить заявку")
+                                .append(System.lineSeparator())
+                                .append("3 - Удалить заявку")
+                                .append(System.lineSeparator())
+                                .append("4 - Найти заявку по ID")
+                                .append(System.lineSeparator())
+                                .append("5 - Найти заявку по имени")
+                                .append(System.lineSeparator())
+                                .append("6 - Выйти из программы")
+                                .append(System.lineSeparator())
+                                .append("Заявок нет")
+                                .append(System.lineSeparator())
+                                .append("Меню :")
+                                .append(System.lineSeparator())
+                                .append("0 - Добавить заявку")
+                                .append(System.lineSeparator())
+                                .append("1 - Показать все заявки")
+                                .append(System.lineSeparator())
+                                .append("2 - Изменить заявку")
+                                .append(System.lineSeparator())
+                                .append("3 - Удалить заявку")
+                                .append(System.lineSeparator())
+                                .append("4 - Найти заявку по ID")
+                                .append(System.lineSeparator())
+                                .append("5 - Найти заявку по имени")
+                                .append(System.lineSeparator())
+                                .append("6 - Выйти из программы")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
     }
 
     @Test
@@ -48,66 +91,262 @@ public class StartUITest {
         // создаём StubInput с последовательностью действий
         Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});
         new StartUI(tracker, input).init();
-        // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
-        assertThat(tracker.findAll()[0].getName(), is("test name"));
+        assertThat(
+                new String(out.toByteArray()),
+                Is.is(
+                        new StringBuilder()
+                                .append("Меню :")
+                                .append(System.lineSeparator())
+                                .append("0 - Добавить заявку")
+                                .append(System.lineSeparator())
+                                .append("1 - Показать все заявки")
+                                .append(System.lineSeparator())
+                                .append("2 - Изменить заявку")
+                                .append(System.lineSeparator())
+                                .append("3 - Удалить заявку")
+                                .append(System.lineSeparator())
+                                .append("4 - Найти заявку по ID")
+                                .append(System.lineSeparator())
+                                .append("5 - Найти заявку по имени")
+                                .append(System.lineSeparator())
+                                .append("6 - Выйти из программы")
+                                .append(System.lineSeparator())
+                                .append("---- Добавление новой заявки ----")
+                                .append(System.lineSeparator())
+                                .append("---- Новая заявка с Id :  добавлена")
+                                .append(System.lineSeparator())
+                                .append("Меню :")
+                                .append(System.lineSeparator())
+                                .append("0 - Добавить заявку")
+                                .append(System.lineSeparator())
+                                .append("1 - Показать все заявки")
+                                .append(System.lineSeparator())
+                                .append("2 - Изменить заявку")
+                                .append(System.lineSeparator())
+                                .append("3 - Удалить заявку")
+                                .append(System.lineSeparator())
+                                .append("4 - Найти заявку по ID")
+                                .append(System.lineSeparator())
+                                .append("5 - Найти заявку по имени")
+                                .append(System.lineSeparator())
+                                .append("6 - Выйти из программы")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
     }
 
     @Test
     public void updateItem() {
         Tracker tracker = new Tracker();
-        // Напрямую добавляем заявку
         Item item = tracker.add(new Item("test name", "desc"));
-        // создаём StubInput с последовательностью действий(производим замену заявки)
+        // создаём StubInput с последовательностью действий (производим замену заявки)
         Input input =  new StubInput(new String[]{"2", item.getId(), "test replace", "заменили заявку", "6"});
-        // создаём StartUI и вызываем метод init()
         new StartUI(tracker, input).init();
-        // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
-        assertThat(tracker.findById(item.getId()).getName(), is("test replace"));
+        assertThat(
+                new String(out.toByteArray()),
+                Is.is(
+                        new StringBuilder()
+                                .append("Меню :")
+                                .append(System.lineSeparator())
+                                .append("0 - Добавить заявку")
+                                .append(System.lineSeparator())
+                                .append("1 - Показать все заявки")
+                                .append(System.lineSeparator())
+                                .append("2 - Изменить заявку")
+                                .append(System.lineSeparator())
+                                .append("3 - Удалить заявку")
+                                .append(System.lineSeparator())
+                                .append("4 - Найти заявку по ID")
+                                .append(System.lineSeparator())
+                                .append("5 - Найти заявку по имени")
+                                .append(System.lineSeparator())
+                                .append("6 - Выйти из программы")
+                                .append(System.lineSeparator())
+                                .append("Изменить заявку :")
+                                .append(System.lineSeparator())
+                                .append("Заявка с id: " + tracker.findById(item.getId()).getId() + " была изменена !")
+                                .append(System.lineSeparator())
+                                .append("Меню :")
+                                .append(System.lineSeparator())
+                                .append("0 - Добавить заявку")
+                                .append(System.lineSeparator())
+                                .append("1 - Показать все заявки")
+                                .append(System.lineSeparator())
+                                .append("2 - Изменить заявку")
+                                .append(System.lineSeparator())
+                                .append("3 - Удалить заявку")
+                                .append(System.lineSeparator())
+                                .append("4 - Найти заявку по ID")
+                                .append(System.lineSeparator())
+                                .append("5 - Найти заявку по имени")
+                                .append(System.lineSeparator())
+                                .append("6 - Выйти из программы")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
     }
 
     @Test
     public void deleteItem() {
         Tracker tracker = new Tracker();
-        System.out.println("start " + tracker.findAll());
         // добавляем заявку
         Item item = tracker.add(new Item("test name", "desc"));
-        //создаём StubInput с последовательностью действий(удаляем заявку)
+        //создаём StubInput с последовательностью действий (удаляем заявку)
         Input input =  new StubInput(new String[]{"3", item.getId(), "6"});
-        // создаём StartUI и вызываем метод init()
-        System.out.println("test " + item.getId());
         new StartUI(input).init();
 
         boolean result = tracker.delete(item.getId());
 
-        // проверяем, что заявка удалилась.
-        assertThat(result, is(true));
+        assertThat(
+                new String(out.toByteArray()),
+                Is.is(
+                        new StringBuilder()
+                                .append("Меню :")
+                                .append(System.lineSeparator())
+                                .append("0 - Добавить заявку")
+                                .append(System.lineSeparator())
+                                .append("1 - Показать все заявки")
+                                .append(System.lineSeparator())
+                                .append("2 - Изменить заявку")
+                                .append(System.lineSeparator())
+                                .append("3 - Удалить заявку")
+                                .append(System.lineSeparator())
+                                .append("4 - Найти заявку по ID")
+                                .append(System.lineSeparator())
+                                .append("5 - Найти заявку по имени")
+                                .append(System.lineSeparator())
+                                .append("6 - Выйти из программы")
+                                .append(System.lineSeparator())
+                                .append("Удаление заявки по id :")
+                                .append(System.lineSeparator())
+                                .append("Заявка с id: " + tracker.findById(item.getId()).getId() + " НЕ была удалена !")
+                                .append(System.lineSeparator())
+                                .append("Меню :")
+                                .append(System.lineSeparator())
+                                .append("0 - Добавить заявку")
+                                .append(System.lineSeparator())
+                                .append("1 - Показать все заявки")
+                                .append(System.lineSeparator())
+                                .append("2 - Изменить заявку")
+                                .append(System.lineSeparator())
+                                .append("3 - Удалить заявку")
+                                .append(System.lineSeparator())
+                                .append("4 - Найти заявку по ID")
+                                .append(System.lineSeparator())
+                                .append("5 - Найти заявку по имени")
+                                .append(System.lineSeparator())
+                                .append("6 - Выйти из программы")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
     }
 
     @Test
     public void findByName() {
         Tracker tracker = new Tracker();
-        // Напрямую добавляем заявку
         Item item = tracker.add(new Item("test name", "desc"));
-        //создаём StubInput с последовательностью действий(производим замену заявки)
         Input input =  new StubInput(new String[]{"5", item.getName(), "6"});
         // создаём StartUI и вызываем метод init()
-        // new StartUI(input).init();
         new StartUI(tracker, input).init();
-        // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
-        Item result = tracker.findByName(item.getName())[0];
-        assertThat(result, is(item));
+        assertThat(
+                new String(out.toByteArray()),
+                Is.is(
+                        new StringBuilder()
+                                .append("Меню :")
+                                .append(System.lineSeparator())
+                                .append("0 - Добавить заявку")
+                                .append(System.lineSeparator())
+                                .append("1 - Показать все заявки")
+                                .append(System.lineSeparator())
+                                .append("2 - Изменить заявку")
+                                .append(System.lineSeparator())
+                                .append("3 - Удалить заявку")
+                                .append(System.lineSeparator())
+                                .append("4 - Найти заявку по ID")
+                                .append(System.lineSeparator())
+                                .append("5 - Найти заявку по имени")
+                                .append(System.lineSeparator())
+                                .append("6 - Выйти из программы")
+                                .append(System.lineSeparator())
+                                .append("Поиск по имени заявки :")
+                                .append(System.lineSeparator())
+                                .append("Заявка с именем > test name < найдена !")
+                                .append(System.lineSeparator())
+                                .append(tracker.findByName("test name"))
+                                .append(System.lineSeparator())
+                                .append("Меню :")
+                                .append(System.lineSeparator())
+                                .append("0 - Добавить заявку")
+                                .append(System.lineSeparator())
+                                .append("1 - Показать все заявки")
+                                .append(System.lineSeparator())
+                                .append("2 - Изменить заявку")
+                                .append(System.lineSeparator())
+                                .append("3 - Удалить заявку")
+                                .append(System.lineSeparator())
+                                .append("4 - Найти заявку по ID")
+                                .append(System.lineSeparator())
+                                .append("5 - Найти заявку по имени")
+                                .append(System.lineSeparator())
+                                .append("6 - Выйти из программы")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
     }
 
     @Test
     public void findByID() {
         Tracker tracker = new Tracker();
-        // Напрямую добавляем заявку
         Item item = tracker.add(new Item("test name", "desc"));
-        // создаём StubInput с последовательностью действий(производим замену заявки)
         Input input =  new StubInput(new String[]{"4", item.getId(), "6"});
-        // создаём StartUI и вызываем метод init()
         new StartUI(input).init();
         // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
-        assertThat(tracker.findById(item.getId()), is(item));
+        assertThat(
+                new String(out.toByteArray()),
+                Is.is(
+                        new StringBuilder()
+                                .append("Меню :")
+                                .append(System.lineSeparator())
+                                .append("0 - Добавить заявку")
+                                .append(System.lineSeparator())
+                                .append("1 - Показать все заявки")
+                                .append(System.lineSeparator())
+                                .append("2 - Изменить заявку")
+                                .append(System.lineSeparator())
+                                .append("3 - Удалить заявку")
+                                .append(System.lineSeparator())
+                                .append("4 - Найти заявку по ID")
+                                .append(System.lineSeparator())
+                                .append("5 - Найти заявку по имени")
+                                .append(System.lineSeparator())
+                                .append("6 - Выйти из программы")
+                                .append(System.lineSeparator())
+                                .append("Поиск заявки по id:")
+                                .append(System.lineSeparator())
+                                .append("Заявка с id > " + tracker.findById(item.getId()).getId() + " < НЕ найдена !")
+                                .append(System.lineSeparator())
+                                .append("Меню :")
+                                .append(System.lineSeparator())
+                                .append("0 - Добавить заявку")
+                                .append(System.lineSeparator())
+                                .append("1 - Показать все заявки")
+                                .append(System.lineSeparator())
+                                .append("2 - Изменить заявку")
+                                .append(System.lineSeparator())
+                                .append("3 - Удалить заявку")
+                                .append(System.lineSeparator())
+                                .append("4 - Найти заявку по ID")
+                                .append(System.lineSeparator())
+                                .append("5 - Найти заявку по имени")
+                                .append(System.lineSeparator())
+                                .append("6 - Выйти из программы")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
     }
 }
